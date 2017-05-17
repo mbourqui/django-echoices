2c507d7a9a39219616afa4018e4a181467a1d58b
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-05-17 11:05:08 +0200

    Add doc and init of _value2member_map_ as in enum

--- echoices/enums.py
+++ echoices/enums.py
@@ -37,12 +37,29 @@ class EChoice(Enum):
 
     @classmethod
     def values(cls):
+        """
+        
+        Returns
+        -------
+        tuple
+            of all the values of this Enum
+        
+        """
         if not hasattr(cls, '__values_'):
             cls.__values_ = tuple([c.value for c in list(cls)])
         return cls.__values_
 
     @classmethod
     def max_value_length(cls):
+        """
+        Not to be used when using numeric values.
+        
+        Returns
+        -------
+        int
+            the maximal length required by this Enum to be stored in the database
+
+        """
         if not hasattr(cls, '__max_value_length_'):
             cls.__max_value_length_ = max([len(c.value) for c in list(cls)])
         return cls.__max_value_length_
@@ -88,7 +105,7 @@ class EChoice(Enum):
         except AttributeError:
             value2member_map_ = {}
             for echoice in list(cls):
-                value2member_map_.update({echoice.value: echoice})
+                value2member_map_[echoice.value] = echoice
             cls._value2member_map_ = value2member_map_
             return cls._value2member_map_[value]
 
