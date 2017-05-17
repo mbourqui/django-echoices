96a71ce5e62c4b10c1f3bada89b4f05c1df27708
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-05-17 11:04:58 +0200

    Tests for EChoice

--- echoices/tests/models.py
+++ echoices/tests/models.py
@@ -3,10 +3,32 @@ from django.db import models
 from echoices.enums import EChoice
 
 
-class TestModelEChoiceChar(models.Model):
-    class ETestChoices(EChoice):
-        FIELD1 = ('a', 'Value 1')
-        FIELD2 = ('b', 'Value 2')
+class ETestCharChoices(EChoice):
+    FIELD1 = ('v', 'Label 1')
+    FIELD2 = ('w', 'Label 2')
 
-    choices_charfield = models.CharField(max_length=1, choices=ETestChoices.choices(),
-                                         default=ETestChoices.FIELD1.value)
+
+class ETestStrChoices(EChoice):
+    FIELD1 = ('value1', 'Label 1')
+    FIELD2 = ('value2', 'Label 2')
+
+
+class ETestIntChoices(EChoice):
+    FIELD1 = (10, 'Label 1')
+    FIELD2 = (20, 'Label 2')
+
+
+class TestModelEChoiceChar(models.Model):
+    choices_charfield = models.CharField(max_length=ETestCharChoices.max_value_length(),
+                                         choices=ETestCharChoices.choices(),
+                                         default=ETestCharChoices.FIELD1.value)
+
+
+class TestModelEChoiceStr(models.Model):
+    choices_charfield = models.CharField(max_length=ETestStrChoices.max_value_length(),
+                                         choices=ETestStrChoices.choices(),
+                                         default=ETestStrChoices.FIELD1.value)
+
+
+class TestModelEChoiceInt(models.Model):
+    choices_charfield = models.IntegerField(choices=ETestIntChoices.choices(), default=ETestIntChoices.FIELD1.value)
