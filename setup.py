ae8479cfd85d50edb415bb6dafdb31f498c48cfb
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-05-24 09:29:28 +0200

    Fix package metadata due to drop of Django 1.8

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
