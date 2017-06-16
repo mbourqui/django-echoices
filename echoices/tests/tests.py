8ee4159be0fce8974a9780f6975fb3f80b5f57df
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-06-16 15:06:08 +0200

    Add assertion for selected tag presence in form from instance

--- echoices/tests/tests.py
+++ echoices/tests/tests.py
@@ -484,6 +484,16 @@ class AdminTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertInHTML('<option value="u" selected="selected">Label 1</option>', response.rendered_content)
 
+    def test_admin_testechoicefieldestrchoicesmodel_list(self):
+        TestEChoiceFieldEStrChoicesModel.objects.create(choice=ETestStrChoices.FIELD1)
+        objs = TestEChoiceFieldEStrChoicesModel.objects.all()
+        print(objs)
+        for obj in objs:
+            print(obj)
+        for param in ['', ]:
+            response = self.client.get('/admin/tests/testechoicefieldestrchoicesmodel/' + param)
+            self.assertEqual(response.status_code, 200)
+
 
 class FormTest(TestCase):
     def test_form(self):
@@ -521,3 +531,6 @@ class FormTest(TestCase):
 
         f = TestEChoiceFieldEStrChoicesModelForm(dict(choice=ETestStrChoices.FIELD1))
         self.assertTrue(f.save())
+        instance = TestEChoiceFieldEStrChoicesModel.objects.get(pk=1)
+        f = TestEChoiceFieldEStrChoicesModelForm(instance=instance)
+        self.assertInHTML('<option value="value1" selected="selected">Label 1</option>', str(f))
