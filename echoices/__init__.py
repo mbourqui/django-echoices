bf2e20b284b05b4eca3614855a1fb5095399cadd
Marc Bourqui <marc.bourqui@agroscope.admin.ch>
2017-07-07 18:42:01 +0200

    Add license disclaimer in __init__ and provide shortcuts

--- echoices/__init__.py
+++ echoices/__init__.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Django EChoices, choices for Django model fields as enumeration
+Copyright (C) 2017  Marc Bourqui
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from .enums import EChoice, EOrderedChoice, EAutoChoice
+from .fields import make_echoicefield
+from .forms import TypedEChoiceField
 
 __license__ = 'GNU GPLv3'
 __author__ = 'Marc Bourqui <https://github.com/mbourqui>'
