55f9df6413df9e8e845fd1cd3e3d62fd022c7290
Marc Bourqui <marc@bourqui.org>
2020-05-17 23:03:53 +0200

    Fix test settings for Django>=2

--- echoices/tests/settings.py
+++ echoices/tests/settings.py
@@ -10,6 +10,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.sessions',
 
     'echoices.tests',
