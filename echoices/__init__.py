a28ec182d73fe9188acc1b02e5bfaa3358a249b4
Marc Bourqui <mbourqui@users.noreply.github.com>
2021-08-18 19:28:25 +0200

    Update license references

--- echoices/__init__.py
+++ echoices/__init__.py
@@ -2,23 +2,23 @@
 # -*- coding: utf-8 -*-
 """
 django-echoices, choices for Django model fields as enumeration
-Copyright (C) 2017  Marc Bourqui
+Copyright (C) 2021  Marc Bourqui
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the MIT License as published by the 
+Massachusetts Institute of Technology.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MIT License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the MIT License
+along with this program.  If not, see 
+<https://opensource.org/licenses/MIT>.
 """
 
-__license__ = 'GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.html>'
+__license__ = 'MIT'
 __author__ = 'Marc Bourqui <pypi.kemar@bourqui.org>'
 __version__ = '2.8.0'
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
